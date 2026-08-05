9afcf03ceb32e05e4efef469d5ca4901c514dda1
Rodrigo <159104466+RexTheBeast@users.noreply.github.com>
2026-08-05 01:15:00 -0400

    Lesson4 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 
-const todos = [
-        {id: 1, title: "Do Chores"},
-        {id: 2, title: "Exerices"},
-        {id: 3, title: "Do Homework"},
-    ]
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle){
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle
+    };
+
+    setTodoList(previous => [newTodo, ...previous]);
+  }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm></TodoForm>
+      <TodoForm onAddTodo={addTodo}></TodoForm>
       <TodoList todoList={todoList}></TodoList>
     </div>
   )
